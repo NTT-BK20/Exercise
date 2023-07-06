7f35ca00f40d2a4edb9d65fc1d8ea4f1f539956d
TuanNT1 <TuanNT1@dc.local>
2023-07-06 17:25:15 +0700

    Update Password_Application.cpp

--- Password_Application/Password_Application.cpp
+++ Password_Application/Password_Application.cpp
@@ -1,4 +1,4 @@
-// Password_Application.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// Password_Application.cpp : This file contains the 'main' function. Program execution begins and ends there.
 /*
  * Password_Application.cpp
  *
@@ -10,17 +10,22 @@
 #include <cstdlib> //for rand() and srand()
 #include <ctime> //for time()
 #include <string>
+#include <chrono>
+#include <thread>
 
 using namespace std;
-using std::system;
 
 #define passwordSpecialCharacters pwSC
+
+void delay(int seconds) {
+    this_thread::sleep_for(chrono::seconds(seconds));
+}
 
 // Ham nhap Password
 class Password
 {
 public:
-    Password(int l, bool up = false, bool low = false, bool num = false, bool sc = false)
+    Password(int l = 0, bool up = false, bool low = false, bool num = false, bool sc = false)
         : pwLenght(l), pwUppercase(up), pwLowercase(low), pwNumbers(num), pwSC(sc)
     {}
 
@@ -39,30 +44,59 @@ public:
         cout << "\n" << "==============================================================" << endl;
         cout << "\n" << "Hay nhap lua chon cua ban: ";
         cin >> chooseMenu;
+        cin.ignore();
+        while (cin.fail())
+        {
+            cin.clear(); // Dat lai trang thai
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Bo qua cac ky tu khong hop le
+
+            cout << "\nBan da nhap khong phai so nguyen. Vui long thu lai!\n\nNhan Enter de tiep tuc!";
+            getchar();
+            return chooseMenu = 0;
+        }
         return chooseMenu;
     }
 
-    int nhapLenght()
+    int inputLenght()
     {
         cout << "\n1. Nhap do dai cua password: ";
         cin >> pwLenght;
-        if (pwLenght < 8)
+        cin.ignore();
+        if (cin.fail())
         {
-            cout << "Vui long lua chon do dai password dai hon 8 ki tu !\n" <<
-                    "\nNhan phim bat ki de tiep tuc!" << endl;
+            cin.clear(); // Dat lai trang thai
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Bo qua cac ky tu khong hop le
+            cout << "\nBan da nhap khong phai so nguyen. Vui long thu lai!\n\nNhan Enter de tiep tuc!";
+            getchar();
             return pwLenght = 0;
-            system("pause");
+        }
+        else if ( pwLenght < 8 )
+        {
+            cout << "Vui long nhap do dai password toi thieu 8 ki tu!\n\nNhan Enter de tiep tuc!";
+            
+            // Dung chuong trinh thay cho system("pause")
+            getchar();
+            
+            return pwLenght = 0;
         }
         else return pwLenght;
     }
 
-    bool nhapUppercase()
+    bool inputUppercase()
     {
         int n; // Lua chon Yes or No
-        cout << "\n" << "2. Ban co muon chu IN HOA khong?" << "\n" <<
-        "1. Yes      2. No" << endl;
+        cout << "\n" << "2. Ban co muon chu IN HOA khong?\n1. Yes      2. No" << endl;
         cout << "Hay nhap lua chon cua ban: ";
         cin >> n;
+        cin.ignore();
+        if (cin.fail())
+        {
+            cin.clear(); // Dat lai trang thai
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Bo qua cac ky tu khong hop le
+            cout << "\nBan da nhap khong phai so nguyen. Vui long thu lai!\n\nNhan Enter de tiep tuc!";
+            getchar();
+            return pwUppercase = false;
+        }
         switch (n)
         {
         case 1:
@@ -73,19 +107,27 @@ public:
             break;
         default:
             cout << "Xin hay chon lai!\n" <<
-                    "\nNhan phim bat ki de tiep tuc!" << endl;
-            system("pause");
+                "\nNhan Enter de tiep tuc!";
+            getchar();
             break;
         }
     }
 
-    bool nhapLowercase()
+    bool inputLowercase()
     {
         int n; // Lua chon Yes or No
-        cout << "\n" << "3. Ban co muon chu viet thuong khong?" << "\n" <<
-        "1. Yes      2. No" << endl;
+        cout << "\n" << "3. Ban co muon chu viet thuong khong?\n1. Yes      2. No" << endl;
         cout << "Hay nhap lua chon cua ban: ";
         cin >> n;
+        cin.ignore();
+        if (cin.fail())
+        {
+            cin.clear(); // Dat lai trang thai
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Bo qua cac ky tu khong hop le
+            cout << "\nBan da nhap khong phai so nguyen. Vui long thu lai!\n\nNhan Enter de tiep tuc!";
+            getchar();
+            return pwLowercase = false;
+        }
         switch (n)
         {
         case 1:
@@ -96,19 +138,27 @@ public:
             break;
         default:
             cout << "Xin hay chon lai!\n" <<
-                    "\nNhan phim bat ki de tiep tuc!" << endl;
-            system("pause");
+                "\nNhan Enter de tiep tuc!";
+            getchar();
             break;
         }
     }
 
-    bool nhapNumbers()
+    bool inputNumbers()
     {
         int n; // Lua chon Yes or No
-        cout << "\n" << "4. Ban co muon chu so khong?" << "\n" <<
-        "1. Yes      2. No" << endl;
+        cout << "\n" << "4. Ban co muon chu so khong?\n1. Yes      2. No" << endl;
         cout << "Hay nhap lua chon cua ban: ";
         cin >> n;
+        cin.ignore();
+        if (cin.fail())
+        {
+            cin.clear(); // Dat lai trang thai
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Bo qua cac ky tu khong hop le
+            cout << "\nBan da nhap khong phai so nguyen. Vui long thu lai!\n\nNhan Enter de tiep tuc!";
+            getchar();
+            return pwLowercase = false;
+        }
         switch (n)
         {
         case 1:
@@ -118,21 +168,28 @@ public:
             return pwNumbers = false;
             break;
         default:
-            cout << "Xin hay chon lai!\n" <<
-                    "\nNhan phim bat ki de tiep tuc!" << endl;
-            system("pause");
+            cout << "Xin hay chon lai!\nNhan Enter de tiep tuc!";
+            getchar();
             break;
         }
     }
 
     // Nhap Speacial Characters
-    bool nhapSC()
+    bool inputSC()
     {
         int n; // Lua chon Yes or No
-        cout << "\n" << "5. Ban co muon ki tu dac biet khong?" << "\n" <<
-        "1. Yes      2. No" << endl;
+        cout << "\n" << "5. Ban co muon ki tu dac biet khong?\n1. Yes      2. No" << endl;
         cout << "Hay nhap lua chon cua ban: ";
         cin >> n;
+        cin.ignore();
+        if (cin.fail())
+        {
+            cin.clear(); // Dat lai trang thai
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Bo qua cac ky tu khong hop le
+            cout << "\nBan da nhap khong phai so nguyen. Vui long thu lai!\n\nNhan Enter de tiep tuc!";
+            getchar();
+            return pwSC = false;
+        }
         switch (n)
         {
         case 1:
@@ -142,11 +199,90 @@ public:
             return pwSC = false;
             break;
         default:
-            cout << "Xin hay chon lai!\n" <<
-                "\nNhan phim bat ki de tiep tuc!" << endl;
-            system("pause");
+            cout << "Xin hay chon lai!\n\nNhan Enter de tiep tuc!";
+            getchar();
             break;
+        }    
+    }
+
+    string randPass()
+    {
+        srand(time(NULL));
+        string showPass;
+        int i = 0;
+        int flag1 = 0, flag2 = 0, flag3 = 0, flag4 = 0; // Check xem mat khau da chua ki tu nay chua ?
+        LOOP: while (i < pwLenght)
+        {
+            if (pwUppercase == true)
+            {
+                flag1 == 0 ? flag1 = 0 : flag1 = rand() % 2;
+                switch (flag1)
+                {
+                case 0:
+                    showPass += char(rand() % 26 + 65);
+                    i++;
+                    flag1 = 1; // Da chua ki tu
+                    break;
+                case 1: goto LOOP;
+                }
+            }
+
+            if (pwLowercase == true)
+            {
+                flag2 == 0 ? flag2 = 0 : flag2 = rand() % 2;
+                switch (flag2)
+                {
+                case 0:
+                    showPass += char(rand() % 26 + 97);
+                    i++;
+                    flag2 = 1;
+                    break;
+                case 1: goto LOOP;
+                }
+            }
+
+            if (pwNumbers == true)
+            {
+                flag3 == 0 ? flag3 = 0 : flag3 = rand() % 2;
+                switch (flag3)
+                {
+                case 0:
+                    showPass += char(rand() % 10 + 48);
+                    i++;
+                    flag3 = 1;
+                    break;
+                case 1: goto LOOP;
+                }
+            }
+
+            if (pwSC == true)
+            {
+                flag4 == 0 ? flag4 = 0 : flag4 = rand() % 2;
+                switch (flag4)
+                {
+                case 0:
+                    showPass += ((rand() % 2) == 0 ? char(rand() % 15 + 33) : char(rand() % 7 + 58)); // Bo ki tu dac biet trong ASCII co 2 bo
+                    flag4 = 1;
+                    i++;
+                    break;
+                case 1: goto LOOP;
+                }
+            }
         }
+        // Xao tron mat khau
+        int r = 0; // rand
+        for (int j = 0; j < pwLenght; j++)
+        {
+            r = rand() % pwLenght;
+            if (showPass[j] != showPass[r])
+            {
+                int temp = showPass[j];
+                showPass[j] = showPass[r];
+                showPass[r] = temp;
+            }
+            else j--;
+        }
+        return showPass;
     }
 
 private: // pw = password
@@ -157,119 +293,36 @@ private: // pw = password
     bool pwSC; // ki tu dac biet
 };
 
-class XuLi_Pass : public Password
-{
-public:
-    XuLi_Pass(int l, bool up, bool low, bool num, bool sc)
-        :Password(l)
-    {
-        srand(time(NULL));
-        string showPass;
-        int i = 0; 
-        int flag1 = 0, flag2 = 0, flag3 = 0, flag4 = 0; // Check xem mat khau da chua ki tu nay chua ?
-        LOOP: while (i < l)
-        {
-        if (up == true)
-        {
-            flag1 == 0 ? flag1 = 0 : flag1 = rand() % 2;
-            switch (flag1)
-            {
-            case 0:
-                showPass += char(rand() % 26 + 65);
-                i++;
-                flag1 = 1; // Da chua ki tu
-                break;
-            case 1: goto LOOP;
-            }
-        }
-
-        if (low == true)
-        {
-            flag2 == 0 ? flag2 = 0 : flag2 = rand() % 2;
-            switch (flag2)
-            {
-            case 0:
-                showPass += char(rand() % 26 + 97);
-                i++;
-                flag2 = 1;
-                break;
-            case 1: goto LOOP;
-            }
-        }
-
-        if (num == true)
-        {
-            flag3 == 0 ? flag3 = 0 : flag3 = rand() % 2;
-            switch (flag3)
-            {
-            case 0:
-                showPass += char(rand() % 10 + 48);
-                i++;
-                flag3 = 1;
-                break;
-            case 1: goto LOOP;
-            }
-        }
-
-        if (sc == true)
-        {
-            flag4 == 0 ? flag4 = 0 : flag4 = rand() % 2;
-            switch (flag4)
-            {
-            case 0:
-                showPass += ( (rand() % 2) == 0 ? char(rand() % 15 + 33) : char(rand() % 7 + 58) ); // Bo ki tu dac biet trong ASCII co 2 bo
-                flag4 = 1;
-                i++;
-                break;
-            case 1: goto LOOP;
-            }
-        }
-    }
-    // Xao tron mat khau
-    int r; // rand
-    for (int j = 0; j < l; j++)
-    {
-        r = rand() % l;
-        if (showPass[j] != showPass[r])
-        {
-            int temp = showPass[j];
-            showPass[j] = showPass[r];
-            showPass[r] = temp;
-        }
-        else j--;
-    }
-    cout << "\nGenerated password: " << showPass << "\n" << endl;
-    }
-};
-
 int main()
 {
     int l = 0; 
     bool up = false, low = false, num = false, sc = false;
-    Password* pw = new Password(0);
-    XuLi_Pass* xl_pw = nullptr;
+    Password* pw = new Password();
     while (true)
     {
         switch (pw->showMenu())
         {
+        case 0:
+            system("cls");
+            break;
         case 1:
-            l = pw->nhapLenght();
+            l = pw->inputLenght();
             system("cls");
             break;
         case 2:
-            up = pw->nhapUppercase();
+            up = pw->inputUppercase();
             system("cls");
             break;
         case 3:
-            low = pw->nhapLowercase();
+            low = pw->inputLowercase();
             system("cls");
             break;
         case 4:
-            num = pw->nhapNumbers();
+            num = pw->inputNumbers();
             system("cls");
             break;
         case 5:
-            sc = pw->nhapSC();
+            sc = pw->inputSC();
             system("cls");
             break;
         case 6:
@@ -277,39 +330,38 @@ int main()
             {
                 cout << "\nVui long lua chon do dai password dai hon 8 ki tu !\n" <<
                         "Mat khau nen co chu IN HOA, chu thuong, chu so, ki tu dac biet !\n" <<
-                        "\nNhan phim bat ki de tiep tuc!" << endl;
-                system("pause");
+                        "\nNhan Enter de tiep tuc!";
+                getchar();
                 system("cls");
                 break;
             }
             else if (up == false && low == false && num == false && sc == false)
             {
-                cout << "Vui long chon mat khau co cac dieu kien sau day: \n" <<
-                        "chu IN HOA, chu thuong, chu so, ki tu dac biet ! \n" <<
-                        "\nNhan phim bat ki de tiep tuc!" << endl;
-                system("pause");
+                cout << "\nVui long chon mat khau co cac dieu kien sau day:" <<
+                        "\nchu IN HOA, chu thuong, chu so, ki tu dac biet ! \n" <<
+                        "\nNhan Enter de tiep tuc!";
+                getchar();
                 system("cls");
                 break;
             }
             else if (l < 8)
             {
-                cout << "Vui long lua chon do dai password dai hon 8 ki tu !\n" <<
-                    "\nNhan phim bat ki de tiep tuc!" << endl;
-                system("pause");
+                cout << "Vui long lua chon do dai password dai hon 8 ki tu !\n\nNhan Enter de tiep tuc!";
+                getchar();
                 system("cls");
                 break;
             }
             else
             {
-                xl_pw = new XuLi_Pass(l,up,low,num,sc);
-                system("pause");
+                cout << "\nGenerated password: " << pw->randPass() << "\n\nNhan Enter de tiep tuc!";
+                getchar();
                 system("cls");
                 break;
             }
+
         default:
-            cout << "Hay chon menu tu 1 -> 6 !\n" <<
-                    "\nNhan phim bat ki de tiep tuc!" << endl;
-            system("pause");
+            cout << "Hay chon menu tu 1 -> 6 !\n\nNhan Enter de tiep tuc!";
+            getchar();
             system("cls");
             break;
         }
